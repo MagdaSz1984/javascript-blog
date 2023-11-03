cf178ba5d9eb6042aa4175a575af0e4362fbcee3
MagdaSz1984 <148221916+MagdaSz1984@users.noreply.github.com>
2023-11-03 13:00:49 +0100

    add class active

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
 "use strict";
 
 function titleClickHandler(event) {
+  const clickedElement = this;
   console.log("Link was clicked!");
-  console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
+
   const activeLinks = document.querySelectorAll(".titles a.active");
 
   for (let activeLink of activeLinks) {
     activeLink.classList.remove("active");
   }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  clickedElement.classList.add("active");
+  console.log("clickedElement:", clickedElement);
 
+  /* [DONE] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll(".posts article.active");
 
-
-  /* remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll(".posts article.active");
-
-    for (let activeArticle of activeArticles) {
-      activeArticle.classList.remove("active");
-    }
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove("active");
+  }
 
   /* get 'href' attribute from the clicked link */
 
@@ -33,12 +34,4 @@ const links = document.querySelectorAll(".titles a");
 
 for (let link of links) {
   link.addEventListener("click", titleClickHandler);
-}
-
-let abecadlo = ["A", "B", "C", "D", "E", "F"];
-
-for (let literka in abecadlo) {
-  console.log("teraz pokazuje literka " + literka);
-  console.log("teraz pokazuje literka " + abecadlo[literka]);
-  // abecadlo[literka] + " Szyma";
 }
