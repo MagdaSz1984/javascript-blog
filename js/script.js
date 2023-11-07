e5de041a217bef02ea9f3c334d80046dd6be9117
MagdaSz1984 <148221916+MagdaSz1984@users.noreply.github.com>
2023-11-07 20:06:43 +0100

    bug fix

--- js/script.js
+++ js/script.js
@@ -36,8 +36,51 @@ function titleClickHandler(event) {
   targetArtickle.classList.add("active");
 }
 
-const links = document.querySelectorAll(".titles a");
+const optArticleSelector = ".post";
+const optTitleSelector = ".post-title";
+const optTitleListSelector = ".titles";
 
-for (let link of links) {
-  link.addEventListener("click", titleClickHandler);
+function generateTitleLinks() {
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  console.log(titleList);
+  titleList.innerHTML = "";
+
+  /*  for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+  console.log(articles);
+
+  let html = "";
+
+  for (let article of articles) {
+    /* get the article id */
+    const articleId = article.getAttribute("id");
+    console.log(articleId);
+
+    /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+    const linkHTML =
+      '<li><a href="#' +
+      articleId +
+      '"><span>' +
+      articleTitle +
+      "</span></a></li>";
+    console.log(linkHTML);
+
+    /* insert link into titleList */
+    html = html + linkHTML;
+  }
+  titleList.innerHTML = html;
+
+  const links = document.querySelectorAll(".titles a");
+
+  for (let link of links) {
+    link.addEventListener("click", titleClickHandler);
+  }
 }
+
+generateTitleLinks();
